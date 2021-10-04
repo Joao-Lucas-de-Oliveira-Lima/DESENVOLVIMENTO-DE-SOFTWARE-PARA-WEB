e0c86f4fe48cdeeb1d9721ec06975d7ac20d2336
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2021-10-03 22:52:19 -0300

    Introducao

---
+++ src/componentes/introducao.jsx
@@ -0,0 +1,14 @@
+
+function introducao(){
+    return (
+        <div>
+            <h1>Nome: João Lucas de Oliveira Lima</h1>
+            <h1>Idade: 22</h1>
+            <h1>Curso: Engenharia de Software</h1>
+            <h1>Cidade de Origem: Pacoti</h1>
+            <h1>E-mail: joaolucasoliveira@alu.ufc.br</h1>
+        </div>
+    )
+}
+
+export default introducao
---
+++ src/index.js
@@ -0,0 +1,6 @@
+//import react from "react"
+import reactDom  from "react-dom"
+import Introducao from "./componentes/introducao"
+
+const root = document.getElementById("root")
+reactDom.render(<Introducao/>, root)
